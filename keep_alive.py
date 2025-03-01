715b11efff38a93e22e6d394b918c2f67051b832
Star Force <cpstarforce@gmail.com>
2025-03-01 08:56:15 +0000

    Improve quality of life

--- keep_alive.py
+++ keep_alive.py
@@ -2,14 +2,23 @@ from flask import Flask
 from threading import Thread
 
 app = Flask('')
-
 @app.route('/')
 def main():
-    return "I'm alive!"
+  return '''
+    <!DOCTYPE html>
+    <html>
+      <head>
+        <title>Discord Keep Alive</title>
+      </head>
+      <body>
+        I'm alive!
+      </body>
+    </html>
+  '''
 
 def run():
-    app.run(host="0.0.0.0", port=8080)
+  app.run(host="0.0.0.0", port=8080)
 
 def keep_alive():
-    server = Thread(target=run)
-    server.start()
+  server = Thread(target=run)
+  server.start()
